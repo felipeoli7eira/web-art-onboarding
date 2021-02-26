95cb1ed4fc9d9fb01fc7cfbd3cf2a597bf15036d
felipeoli7eira <me.felipeoliveira@gmail.com>
2021-02-26 11:48:55 -0300

    setando configuracoes do banco no src/boot/conf.php

--- source/boot/conf-example.php
+++ source/boot/conf-example.php
@@ -2,6 +2,6 @@
 
     /** DATABASE */
     define('CONF_DB_HOST', 'localhost');
-    define('CONF_DB_NAME', 'webartonboarding');
+    define('CONF_DB_NAME', 'app');
     define('CONF_DB_USER', 'root');
-    define('CONF_DB_PASSWORD', 'wsl');
+    define('CONF_DB_PASSWORD', '');
