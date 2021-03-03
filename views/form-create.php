700175b4fc5fd35363e93ced8598b898d9610274
felipeoli7eira <me.felipeoliveira@gmail.com>
2021-03-03 11:07:42 -0300

    Estilo inicial do formulario de criacao de wapper

--- views/form-create.php
+++ views/form-create.php
@@ -2,66 +2,52 @@
     <a href="<?= url() ?>">wappers</a>
 </nav>
 
-<form name="create-wapper" method="POST" action="<?= url('/wapper') ?>">
+<form name="create-wapper" id="create-wapper" method="POST" action="<?= url('/wapper') ?>">
+
+    <div class="input-group">
+        <label for="input-photo">Foto de perfil</label>
+        <input type="file" id="input-photo" name="photo">
+    </div>
 
     <div class="input-group">
         <label for="input-name">Nome</label>
-        <div>
-            <!-- img -->
-            <input type="text" id="input-name" name="name" required minlength="3">
-        </div>
+        <input type="text" id="input-name" name="name" required minlength="3">
     </div>
 
     <div class="input-group">
         <label for="input-birth">Data de nascimento</label>
-        <div>
-            <!-- img -->
-            <input type="date" name="birth" id="input-birth" required>
-        </div>
+        <input type="date" name="birth" id="input-birth" required>
     </div>
 
     <div class="row-form">
         <div class="input-group">
             <label for="input-city">Cidade onde mora</label>
-            <div>
-                <!-- img -->
-                <input type="text" name="city" id="input-city" required>
-            </div>
+            <input type="text" name="city" id="input-city" required>
         </div>
 
         <div class="input-group">
             <label for="input-state">Estado</label>
-            <div>
-                <!-- img -->
-                <input type="text" name="state" id="input-state" required>
-            </div>
+            <input type="text" name="state" id="input-state" required>
         </div>
     </div>
 
     <div class="input-group">
         <label for="input-profession">Profissão</label>
-        <div>
-            <!-- img -->
-            <input type="text" name="profession" id="input-profession" required>
-        </div>
+        <input type="text" name="profession" id="input-profession" required>
     </div>
 
     <div class="input-group">
         <label for="input-email">E-mail</label>
-        <div>
-            <!-- img -->
-            <input type="email" name="email" id="input-email" required>
-        </div>
+        <input type="email" name="email" id="input-email" required>
     </div>
 
     <div class="input-group">
         <label for="input-phone">Número para contato</label>
-        <div>
-            <!-- img -->
-            <input type="number" name="phone" id="input-phone" required>
-        </div>
+        <input type="number" name="phone" id="input-phone" required>
     </div>
 
-    <button type="submit" name="submit">pronto</button>
+    <div class="input-group submit-box">
+        <button type="submit" name="submit">pronto</button>
+    </div>
 
 </form>
