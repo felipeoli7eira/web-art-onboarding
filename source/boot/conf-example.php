ed0f90b57c8806abaaa43d827f10508f95ce064e
felipeoli7eira <me.felipeoliveira@gmail.com>
2021-03-08 17:15:17 -0300

    adicionando arquivo conf-example

---
+++ source/boot/conf-example.php
@@ -0,0 +1,11 @@
+<?php
+
+    /** DATABASE */
+    define('CONF_DB_HOST', 'localhost');
+    define('CONF_DB_NAME', /** sugerido no readme: */ 'onboarding');
+    define('CONF_DB_USER', 'root');
+    define('CONF_DB_PASSWORD', '');
+
+    /** URLs and PATHs */
+    define('CONF_URL_BASE', 'http://localhost/web-art-onboarding');
+    define('CONF_UPLOADS_PATH', __DIR__ . '/../../storage/');
